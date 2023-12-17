05c02db58f68914c94ff9ed5906906754cb41eec
Sudeepa-R <119294212+Sudeepa-R@users.noreply.github.com>
2023-12-17 20:45:39 +0530

    Update speedcheckjs.js

--- speedcheckjs.js
+++ speedcheckjs.js
@@ -83,7 +83,7 @@ function accuracyy(){
     var a=paragraphText.length;
     var b=userInput.length;
     let re=Math.ceil((ac/a)*100);
-    accuracyElement.textContent = 'Accuuracy :'+re+'%';
+    accuracyElement.textContent = 'Accuuracy : '+re+'%';
     document.getElementById('accuracy').style.color="rgb(193, 193, 13)";
 
 }
